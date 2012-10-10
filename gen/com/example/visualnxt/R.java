895e24b92ace8a8d1cbc25471b06fe3471cb7eec
killswitch23456 <killswitch23456@gmail.com>
2012-10-10 14:53:00 -0400

    Merging Data

--- gen/com/example/visualnxt/R.java
+++ gen/com/example/visualnxt/R.java
@@ -18,10 +18,10 @@ public final class R {
     }
     public static final class id {
         public static final int FileList=0x7f070000;
-        public static final int button3=0x7f070006;
-        public static final int button4=0x7f070005;
-        public static final int callCamera=0x7f070004;
-        public static final int callFileDisplay=0x7f070003;
+        public static final int button3=0x7f070005;
+        public static final int button4=0x7f070004;
+        public static final int callCamera=0x7f070003;
+        public static final int callFileDisplay=0x7f070006;
         public static final int imageView1=0x7f070002;
         public static final int menu_settings=0x7f070007;
         public static final int textView1=0x7f070001;
